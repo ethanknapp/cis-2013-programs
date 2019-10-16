12dcd2da7607b857c1cccecf6bee61a17b8c6463
ethanknapp <54726213+ethanknapp@users.noreply.github.com>
2019-10-16 12:40:36 -0500

    Add files via upload

---
+++ tax_calc.js
@@ -0,0 +1,61 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+};
+
+var calculateIncome = function () 
+{
+    var floatAnnualIncome = parseInt($("annualIncome").value);
+    var intUserAge = parseInt($("userAge").value);
+    var floatTaxRate, floatAnnualTax;
+if (intUserAge >= 65)
+    {
+        if (floatAnnualIncome <= 30000)
+        {
+    		floatTaxRate = 2.5;
+    	}
+    	else
+            {
+    		if(floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
+            {
+                floatTaxRate = 5;
+    		}
+    		else
+    		{
+    			floatTaxRate = 20;
+            }
+    	}
+    } 
+    else
+    {
+    	if (floatAnnualIncome <= 30000)
+    	{
+    		floatTaxRate = 5;
+    	}
+   		if (floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
+   		{
+   			floatTaxRate = 10;
+   		}
+   		else
+   		{
+   			floatTaxRate = 20;
+   		}
+	}
+};
+
+	floatAnnualTax = intAnnualIncome * (floatTaxRate/100);
+
+	$("annualTax").value = floatAnnualTax;
+
+
+	alert ("Your annual income tax this year is $" + floatAnnualTax  +"\nBased upon an annual income of $"+ intAnnualIncome+
+	"\nAnd a tax rate of "+ floatTaxRate*100+ "% \nNote: Tax rate is based upon your age");
+
+window.onload = function () 
+{
+    $("annualIncome").value = "";
+    $("userAge").value = "";
+    $("annualTax").value = "";
+    $("calculate").onclick = calculateIncome;
+    $("annualIncome").focus();
+};

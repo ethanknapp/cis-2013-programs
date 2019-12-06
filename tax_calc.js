7b54bef4742f45c541cd1cdc475531091a6a31a9
ethanknapp <54726213+ethanknapp@users.noreply.github.com>
2019-12-06 13:31:44 -0600

    Update tax_calc.js

--- tax_calc.js
+++ tax_calc.js
@@ -5,56 +5,60 @@ var $ = function (id)
 
 var calculateIncome = function () 
 {
-    var floatAnnualIncome = parseInt($("annualIncome").value);
-    var intUserAge = parseInt($("userAge").value);
+    var intAnnualIncome = parseInt($("annualIncome").value);
+    var intUserAge = parseInt($("age").value);
     var floatTaxRate, floatAnnualTax;
-if (intUserAge >= 65)
+    if (intUserAge <= 65)
     {
-        if (floatAnnualIncome <= 30000)
-        {
-    		floatTaxRate = 2.5;
+    	if (intAnnualIncome <= 30000)
+    	{
+    		floatTaxRate = 5;
     	}
     	else
-            {
-    		if(floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
-            {
-                floatTaxRate = 5;
+    	{
+    		if(intAnnualIncome >= 30000 && intAnnualIncome <= 75000)
+    		{
+    			floatTaxRate = 10;
     		}
     		else
     		{
     			floatTaxRate = 20;
             }
-    	}
-    } 
+        }
+    }
     else
     {
-    	if (floatAnnualIncome <= 30000)
+    	if (intAnnualIncome <= 30000)
     	{
-    		floatTaxRate = 5;
+    		floatTaxRate = 2.5;
     	}
-   		if (floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
-   		{
-   			floatTaxRate = 10;
-   		}
-   		else
-   		{
-   			floatTaxRate = 20;
-   		}
-	}
-};
+        else
+        {
+            if (intAnnualIncome >= 30000 && intAnnualIncome <= 75000)
+            {
+                floatTaxRate = 5;
+            }
+            else
+            {
+                floatTaxRate = 20;
+            }
+        }
+    }   
 
 	floatAnnualTax = intAnnualIncome * (floatTaxRate/100);
 
 	$("annualTax").value = floatAnnualTax;
 
 
-	alert ("Your annual income tax this year is $" + floatAnnualTax  +"\nBased upon an annual income of $"+ floatAnnualIncome+
-	"\nAnd a tax rate of "+ floatTaxRate + "% \nNote: Tax rate is based upon your age");
+	alert ("Your annual income tax this year is $" + floatAnnualTax  +"\nBased upon an annual income of $"+ intAnnualIncome+
+	"\nAnd a tax rate of " +floatTaxRate+ "% \nNote: Tax rate is based upon your age");
+	
+};
 
 window.onload = function () 
 {
     $("annualIncome").value = "";
-    $("userAge").value = "";
+    $("age").value = "";
     $("annualTax").value = "";
     $("calculate").onclick = calculateIncome;
     $("annualIncome").focus();
